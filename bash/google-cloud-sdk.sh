5dff8468cfc6b4e6f089cd237c07ca9ff5e79ed5
David J. Hamilton <dhamilton@linkedin.com>
2016-06-01 10:59:36 -0700

    Don't assume google cloud is installed
    Also don't assume the username is davidjh

--- bash/google-cloud-sdk.sh
+++ bash/google-cloud-sdk.sh
@@ -1,6 +1,8 @@
+if [ -d "$HOME/apps/google-cloud-sdk/" ]; then
+	# The next line updates PATH for the Google Cloud SDK.
+	source "$HOME/apps/google-cloud-sdk/path.bash.inc"
 
-# The next line updates PATH for the Google Cloud SDK.
-source '/Users/davidjh/apps/google-cloud-sdk/path.bash.inc'
+	# The next line enables shell command completion for gcloud.
+	source "$HOME/apps/google-cloud-sdk/completion.bash.inc"
+fi
 
-# The next line enables shell command completion for gcloud.
-source '/Users/davidjh/apps/google-cloud-sdk/completion.bash.inc'
