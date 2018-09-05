f4ef21e98470cff1424c42d53576c3f441f00c13
David J. Hamilton <dhamilton@linkedin.com>
2018-09-05 13:26:10 -0700

    Improve ts; now it uses fzf via switch-client

--- bash/Alias/tmux.sh
+++ bash/Alias/tmux.sh
@@ -1,2 +1,4 @@
 alias t='tmux'
-alias ts='tmux choose-session'
+alias ts='tmux list-sessions | fzf  | cut -d':' -f 1 | xargs tmux switch-client -t'
+alias tw='tmux list-windows | fzf  | cut -d':' -f 1 | xargs tmux select-window -t'
+
