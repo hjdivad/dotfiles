c6567442413581484d3e7dc4c1e0eaa78179f777
David J. Hamilton <davidjh@hjdivad.com>
2019-09-08 16:09:35 -0700

    Don't repeat path

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -35,69 +35,6 @@ export NODE_PATH=/usr/local/lib/jsctags/:$NODE_PATH
 
 #}}}
 
-# Path {{{
-
-if [[ -n "$__PATH_RESET" ]]; then
-  path=$__PATH_RESET
-else
-  path=$PATH
-  export __PATH_RESET=$path
-fi
-
-# Android SDK
-if [ -d $HOME/Library/Android/sdk/platform-tools ]; then
-  export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
-fi
-
-if [ -d $HOME/Library/Python/3.7/bin ]; then
-  export PATH=$PATH:$HOME/Library/Python/3.7/bin
-fi
-
-if [ -d /opt/adt/sdk/tools ]; then
-  export PATH=$PATH:/opt/adt/sdk/tools
-fi
-
-if [ -d /opt/adt/eclipse/Eclipse.app/Contents/MacOS ]; then
-  export PATH=$PATH:/opt/adt/eclipse/Eclipse.app/Contents/MacOS
-fi
-
-if [ -d /Library/TeX/texbin ]; then
-  export PATH=$PATH:/Library/TeX/texbin
-fi
-
-if which yarn > /dev/null 2>&1; then
-  # export PATH=$PATH:$(yarn global bin)
-  export PATH=$PATH:$HOME/.yarn/bin # yarn global bin is really slow
-fi
-
-# This variable is used elsewhere to write an os-agnostic grep either with gnu
-# grep installed on macos or it being present on linux systems
-if which ggrep > /dev/null 2>&1; then
-  export GREP=$(which ggrep)
-elif [ -x /usr/bin/grep ]; then
-  export GREP=/usr/bin/grep
-fi
-
-# Use gnu xargs if possible on osx; on other systems the local xargs will
-# already be a reasonable version
-if which gxargs > /dev/null 2>&1; then
-  export XARGS=$(which gxargs)
-elif [ -x /usr/bin/grep ]; then
-  export XARGS=/usr/bin/xargs
-fi
-
-export PATH=/usr/local/bin:$PATH
-export PATH=/usr/local/share/npm/bin:$PATH
-export PATH=$HOME/local/bin:$PATH
-export PATH=$HOME/.cargo/bin:$PATH
-export PATH=$HOME/.notion/bin:$PATH
-export PATH=$HOME/.notion/shim:$PATH
-export PATH=$HOME/.notion:$PATH
-export PATH=$HOME/.dotfiles/bin:$PATH
-export PATH=$HOME/bin:$PATH
-
-# }}}
-
 # Version Mgmt {{{
 
 # initialize rbenv
@@ -105,10 +42,76 @@ if [ -x /usr/local/bin/rbenv ]; then
   eval "$(rbenv init -)"
 fi
 
+if [ -d $HOME/.volta ]; then
+  export VOLTA_HOME=$HOME/.volta
+  source $VOLTA_HOME/load.sh
+fi
+
 # Load RVM, if it is present.
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 #}}}
+
+# Path {{{
+
+if [[ -z "$BASH_PATH_SET_UP" ]]; then
+  # Android SDK
+  if [ -d $HOME/Library/Android/sdk/platform-tools ]; then
+    export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
+  fi
+  
+  if [ -d $HOME/Library/Python/3.7/bin ]; then
+    export PATH=$PATH:$HOME/Library/Python/3.7/bin
+  fi
+  
+  if [ -d /opt/adt/sdk/tools ]; then
+    export PATH=$PATH:/opt/adt/sdk/tools
+  fi
+  
+  if [ -d /opt/adt/eclipse/Eclipse.app/Contents/MacOS ]; then
+    export PATH=$PATH:/opt/adt/eclipse/Eclipse.app/Contents/MacOS
+  fi
+  
+  if [ -d /Library/TeX/texbin ]; then
+    export PATH=$PATH:/Library/TeX/texbin
+  fi
+  
+  if which yarn > /dev/null 2>&1; then
+    # export PATH=$PATH:$(yarn global bin)
+    export PATH=$PATH:$HOME/.yarn/bin # yarn global bin is really slow
+  fi
+  
+  # This variable is used elsewhere to write an os-agnostic grep either with gnu
+  # grep installed on macos or it being present on linux systems
+  if which ggrep > /dev/null 2>&1; then
+    export GREP=$(which ggrep)
+  elif [ -x /usr/bin/grep ]; then
+    export GREP=/usr/bin/grep
+  fi
+  
+  # Use gnu xargs if possible on osx; on other systems the local xargs will
+  # already be a reasonable version
+  if which gxargs > /dev/null 2>&1; then
+    export XARGS=$(which gxargs)
+  elif [ -x /usr/bin/grep ]; then
+    export XARGS=/usr/bin/xargs
+  fi
+  
+  export PATH=/usr/local/bin:$PATH
+  export PATH=/usr/local/share/npm/bin:$PATH
+  export PATH=$HOME/local/bin:$PATH
+  export PATH=$HOME/.cargo/bin:$PATH
+  export PATH=$HOME/.volta/bin:$PATH
+  export PATH=$HOME/.dotfiles/bin:$PATH
+  export PATH=$HOME/bin:$PATH
+
+  export BASH_PATH_SET_UP=1
+
+fi
+
+# }}}
+
+
 
 # Apps + Aliases {{{
 
