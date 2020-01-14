d0a5cbf088aa824b9f3058d9581d369303838127
David J. Hamilton <dhamilton@linkedin.com>
2020-01-14 11:59:47 -0800

    Switch prompt to starship.rs

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -269,52 +269,8 @@ if [[ $(umask) = "0000" ]]; then
 fi
 
 
-# OPTIMISE: This is the slowest part of my prompt
-# 
-_prompt_git() {
-  local ref=$(git symbolic-ref HEAD 2> /dev/null)
-  if [ -n "$ref" ]; then
-    # We have a git, ask him questions, unless it's disabled
-    if  git config prompt.dontCheckStatus > /dev/null ||\
-      ! (git status -s --ignore-submodules | $GREP -qP .); then
-      # Clean
-      local state_prefix=""
-    else
-      # Dirty
-      local state_prefix="[$red+$reset_color]"
-    fi
-    local branch=${ref#refs/heads/}
-    echo "$state_prefix$green$branch$reset_color "
-  else
-    echo ""
-  fi
-}
-
-_prompt_path() {
-  # ~/full/path
-  local path="~${PWD#$HOME}"
-  echo "$yellow$path$reset_color "
-}
-
-_prompt_host() {
-  echo "$cyan\h:$reset_color "
-}
-
-_prompt_command_indicator() {
-  echo "$blue\$$reset_color "
-}
-
-_prompt_history() {
-  echo "history -a; history -c; history -r"
-}
-
-set_prompt() {
-  PS1="$(_prompt_host)$(_prompt_path)$(_prompt_git)\n$(_prompt_command_indicator)"
-}
-
-PROMPT_COMMAND=set_prompt
-# see https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
-PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+# https://starship.rs/
+eval "$(starship init bash)"
 
 # z jumparound; this must be sourced after $PROMPT_COMMAND is set
 if [ -f "$HOME/src/rupa/z/z.sh" ]; then
