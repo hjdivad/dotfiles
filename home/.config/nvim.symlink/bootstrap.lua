a694c26f33ebc798b56ca4458008003871d03f41
David J. Hamilton <github@hjdivad.com>
2022-05-27 13:30:39 -0700

    Fix bootstrapping

---
+++ home/.config/nvim.symlink/bootstrap.lua
@@ -0,0 +1,3 @@
+-- TODO: instead of having a separate bootstrap file, just have init.lua
+-- auto-install (but not auto-update), at least once or something.
+require('hjdivad/plugins').update_plugins()
