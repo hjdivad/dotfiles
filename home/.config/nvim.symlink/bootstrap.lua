4a77398da4c3501d3eefe09136e0e1ed6c3903c2
David J. Hamilton <github@hjdivad.com>
2022-05-27 14:16:54 -0700

    More CI hacking
    * TSInstallSync all as part of bootstrap
    * Run bootstrap twice
    
    It isn't clear why bootstrap sometimes has installation issues.  Running
    it twice as an experimental hack.

--- home/.config/nvim.symlink/bootstrap.lua
+++ home/.config/nvim.symlink/bootstrap.lua
@@ -1,3 +1,5 @@
 -- TODO: instead of having a separate bootstrap file, just have init.lua
 -- auto-install (but not auto-update), at least once or something.
 require('hjdivad/plugins').update_plugins()
+
+vim.cmd('TSInstallSync all')
