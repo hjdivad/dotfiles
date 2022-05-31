ef4f0bdfb2216b65739c1a35a6240dd1b04604fb
David J. Hamilton <dhamilton@linkedin.com>
2022-05-31 10:34:59 -0700

    Load a machine-local init

--- home/.config/nvim.symlink/init.lua
+++ home/.config/nvim.symlink/init.lua
@@ -9,3 +9,8 @@ init.main {
 -- This only sets up utility functions but doesn't otherwise change options or
 -- do anything
 init.create_debug_functions()
+
+local succ, local_init = pcall(require, 'hjdivad_local')
+if succ then
+  local_init()
+end
