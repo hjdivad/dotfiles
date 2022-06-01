d94657dab4d330c7d2e8e2213e4acfef9230292c
David J. Hamilton <github@hjdivad.com>
2022-06-01 07:11:03 -0700

    squash quit on paq

--- home/.config/nvim.symlink/lua/hjdivad_init.lua
+++ home/.config/nvim.symlink/lua/hjdivad_init.lua
@@ -604,7 +604,7 @@ end
 
 local function create_user_commands()
   vim.api.nvim_create_user_command('HiUpdatePlugins', function()
-    require('hjdivad/plugins').update_plugins()
+    require('hjdivad/plugins').update_plugins {}
   end, { desc = 'Update or install plugins' })
 
   vim.api.nvim_create_user_command('HiResize', function()
