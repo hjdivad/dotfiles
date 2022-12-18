90eddcf7eb1296372cb7f3a39083c24a71d5ae5e
David J. Hamilton <github@hjdivad.com>
2022-12-18 11:04:17 -0800

    Remove deprecated API usage

--- home/.config/nvim.symlink/lua/hjdivad/init.lua
+++ home/.config/nvim.symlink/lua/hjdivad/init.lua
@@ -333,8 +333,7 @@ local function setup_language_servers()
   end
 
   -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
   -- list of configurations at <https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md>
   local lsp = require 'lspconfig'
