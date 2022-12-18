399aebc675cdfb93635f8222138af7aaa2fa32de
David J. Hamilton <github@hjdivad.com>
2022-12-18 11:37:41 -0800

    Make hjdivad/exrc more resilient
    In the after/plugin execution, don't assume that hjdivad/exrc is loadable,
    e.g. if dependencies are missing in a fresh install.

--- home/.config/nvim.symlink/after/plugin/lua/hjdivad/exrc.lua
+++ home/.config/nvim.symlink/after/plugin/lua/hjdivad/exrc.lua
@@ -1,5 +1,11 @@
 local function main()
-  local exrc = require('hjdivad/exrc')
+  local succ, exrc = pcall(require, 'hjdivad/exrc')
+
+  -- TODO: this check is only necessary b/c this isn't managed as a plugin
+  if not succ then
+    print('skipping exrc, dependencies not loaded')
+    return
+  end
 
   exrc.run_if_setup(function()
     exrc.run_exrc()
