504d3aa41e7a19933f4e821bfd1776ec17281d38
David J. Hamilton <github@hjdivad.com>
2023-02-18 11:19:11 -0800

    completion

--- packages/nvim/config/lua/plugins/snippets.lua
+++ packages/nvim/config/lua/plugins/snippets.lua
@@ -1,34 +1,58 @@
 -- TODO: snippets + completion
 -- see https://github.com/folke/lazy.nvim#-plugin-spec
 return {
-  -- { "rafamadriz/friendly-snippets", enabled = false},
+  -- TODO: still not loading my own snippets correctly
+  -- TODO: i think i'm seeing either snippets from friendly-snippets 
+  --  or possibly snippets from the LSP
+  --
+  -- TODO: seee  lua require("luasnip.loaders").edit_snippet_files()
+  { "rafamadriz/friendly-snippets", enabled = false },
   {
     "L3MON4D3/LuaSnip",
     -- dependencies = {
-    --   -- TODO: do i want this?
     --   "rafamadriz/friendly-snippets",
     --   config = function()
     --     require("luasnip.loaders.from_vscode").lazy_load()
     --   end,
     -- },
+    -- I use snippets everywhere so
+    lazy = false,
+    -- does nothing since lazy = false, but if we enable lazy loading, we
+    -- should at least load luasnip when editing snippets files
+    ft = "snippets",
     opts = {
       history = true,
       delete_check_events = "TextChanged",
     },
-  -- stylua: ignore
-  keys = {
-      --TODO: delete existing lsp keymaps; in particular c-k
-      --TODO: see ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/lsp
-      --  init + keymaps
-    {
-      "<C-l>",
-      function()
-        return require('luasnip').expandable() and '<Plug>luasnip-expand-or-jump'
-      end,
-      expr = true, silent = true, mode = { "i", "s" },
-    },
-    { "<C-k>", function() require("luasnip").jump(1) end, mode = { "i", "s" } },
-    { "<C-j>", function() require("luasnip").jump(-1) end, mode = {"i", "s"}  },
+    keys = {
+      -- TODO: this presumably doesn't work because it's not luasnip managing the completion;
+      -- it's cmp that needs the keybinding
+      -- {
+      --   "<C-l>",
+      --   function()
+      --     local ls = require('luasnip')
+      --     if ls.expandable() then
+      --       ls.expand()
+      --     end
+      --   end,
+      --   expr = true,
+      --   silent = true,
+      --   mode = { "i", "s" },
+      -- },
+      {
+        "<C-k>",
+        function()
+          require("luasnip").jump(1)
+        end,
+        mode = { "i", "s" },
+      },
+      {
+        "<C-j>",
+        function()
+          require("luasnip").jump(-1)
+        end,
+        mode = { "i", "s" },
+      },
     },
   },
 }
