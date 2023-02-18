542e7f1b1dbeb7e487d38e1760552af98d18b98f
David J. Hamilton <github@hjdivad.com>
2023-02-18 12:03:25 -0800

    made some progress on snippets

--- packages/nvim/config/lua/config/autocmds.lua
+++ packages/nvim/config/lua/config/autocmds.lua
@@ -2,4 +2,5 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+
 -- TODO: see https://github.com/rwjblue/dotvim/blob/f2dcf5d5964ccdd7dc2cdd008898af7a5eb110d5/lua/config/autocmds.lua#L17-L40
--- packages/nvim/config/lua/plugins/snippets.lua
+++ packages/nvim/config/lua/plugins/snippets.lua
@@ -1,44 +1,32 @@
--- TODO: snippets + completion
 -- see https://github.com/folke/lazy.nvim#-plugin-spec
 return {
-  -- TODO: still not loading my own snippets correctly
-  -- TODO: i think i'm seeing either snippets from friendly-snippets 
-  --  or possibly snippets from the LSP
-  --
-  -- TODO: seee  lua require("luasnip.loaders").edit_snippet_files()
   { "rafamadriz/friendly-snippets", enabled = false },
+  -- TODO: disable lsp snippets
   {
     "L3MON4D3/LuaSnip",
-    -- dependencies = {
-    --   "rafamadriz/friendly-snippets",
-    --   config = function()
-    --     require("luasnip.loaders.from_vscode").lazy_load()
-    --   end,
-    -- },
     -- I use snippets everywhere so
     lazy = false,
     -- does nothing since lazy = false, but if we enable lazy loading, we
     -- should at least load luasnip when editing snippets files
     ft = "snippets",
+    config = function (_plugin, opts)
+      require('luasnip').setup(opts)
+
+      local snippets_path = "~/.config/nvim/snippets"
+      require('luasnip.loaders.from_snipmate').lazy_load({
+        paths = snippets_path
+      })
+      require("luasnip.loaders.from_lua").lazy_load({paths = snippets_path})
+
+      vim.api.nvim_create_user_command('HiEditSnippets', function()
+        require('luasnip.loaders').edit_snippet_files({})
+      end, { desc = 'Edit snippets used in this buffer' })
+    end,
     opts = {
       history = true,
       delete_check_events = "TextChanged",
     },
     keys = {
-      -- TODO: this presumably doesn't work because it's not luasnip managing the completion;
-      -- it's cmp that needs the keybinding
-      -- {
-      --   "<C-l>",
-      --   function()
-      --     local ls = require('luasnip')
-      --     if ls.expandable() then
-      --       ls.expand()
-      --     end
-      --   end,
-      --   expr = true,
-      --   silent = true,
-      --   mode = { "i", "s" },
-      -- },
       {
         "<C-k>",
         function()
