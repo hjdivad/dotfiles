cfc2f12f46259cc391756568c58de1875c84410c
David J. Hamilton <github@hjdivad.com>
2023-02-21 08:17:29 -0800

    keymap to close diagnostics window

--- packages/nvim/config/lua/plugins/diagnostics.lua
+++ packages/nvim/config/lua/plugins/diagnostics.lua
@@ -7,6 +7,7 @@ return {
       { "<leader>xX", false },
       { "<leader>xL", false },
       { "<leader>xQ", false },
+      { "<leader>dq", "<cmd>TroubleClose<cr>", desc = "Close Diagnostics (Trouble)" },
       { "<leader>dd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
       { "<leader>dD", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
     },
