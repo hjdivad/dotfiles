cc063e8947fc1f83edbd4dc5e4da346997180969
David J. Hamilton <github@hjdivad.com>
2023-02-26 11:53:26 -0800

    feat: enable exrc in neovim 0.9.0

--- packages/nvim/config/lua/config/options.lua
+++ packages/nvim/config/lua/config/options.lua
@@ -39,4 +39,12 @@ if vim.env.SSH_TTY ~= nil or vim.env.SSH_CLIENT ~= nil then
   }
 end
 
+
+if vim.fn.has("nvim-0.9.0") == 1 then
+  -- automatically run trusted .nvim.lua .nvimrc .exrc
+  -- :trust <file> to trust
+  -- :trust ++deny/++remove <file>
+  vim.opt.exrc = true
+end
+
 pp = vim.pretty_print
