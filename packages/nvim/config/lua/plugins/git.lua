17f725c40b0f332ba5458684635ad624df19b943
David J. Hamilton <dhamilton@linkedin.com>
2023-03-10 08:14:51 -0800

    feat: improve git diff bindings

--- packages/nvim/config/lua/plugins/git.lua
+++ packages/nvim/config/lua/plugins/git.lua
@@ -1,4 +1,6 @@
 -- https://github.com/folke/lazy.nvim#-plugin-spec
+
+
 return {
   {
     "lewis6991/gitsigns.nvim",
@@ -20,15 +22,30 @@ return {
           vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
         end
 
+        local function preview_hunk()
+          -- call twice to focus on the previe window
+          gs.preview_hunk()
+          gs.preview_hunk()
+        end
+
+        local function update_git_base()
+          local merge_base = vim.fn.system('git merge-base HEAD origin/HEAD')
+          if merge_base and #merge_base > 3 then
+            -- strip newline from the command output
+            gs.change_base(merge_base:sub(1, #merge_base - 1), true)
+          end
+        end
+
         map("n", "]h", gs.next_hunk, "Next Hunk")
         map("n", "[h", gs.prev_hunk, "Prev Hunk")
-        map("n", "<leader>Gj", gs.next_hunk, "Next Hunk")
-        map("n", "<leader>Gk", gs.prev_hunk, "Prev Hunk")
-        map("n", "<leader>Gp", gs.preview_hunk, "Preview Hunk")
-        map("n", "<leader>Gb", function()
+        map("n", "<leader>gj", gs.next_hunk, "Next Hunk")
+        map("n", "<leader>gk", gs.prev_hunk, "Prev Hunk")
+        map("n", "<leader>gp", preview_hunk, "Preview Hunk")
+        map("n", "<leader>gb", function()
           gs.blame_line({ full = true })
         end, "Blame Line")
         map("n", "<leader>Gd", gs.diffthis, "Diff This")
+        map("n", "<leader>GB", update_git_base, "Set git base to --merge-base")
         map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
         map({ "o", "x" }, "ah", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
       end,
@@ -46,6 +63,12 @@ return {
         silent = true,
         desc = "Yank GitHub permalink to clipboard",
       },
+      {
+        "<leader>GD",
+        "<Cmd>Gvdiffsplit!<Cr>",
+        mode = "n",
+        desc = "Diff this conflict"
+      }
     },
   },
 }
