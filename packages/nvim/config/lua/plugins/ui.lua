87342b4f933c7b39fcc692c7618b2ffc9166bac8
David J. Hamilton <github@hjdivad.com>
2023-04-20 08:11:06 -0700

    feat: redirect msg_show to popup
    Noice has some sophisticated message routing.  Unfortunately some of the
    defaults go to useless "notify" style views, in particular for things
    like `:Inspect`.
    
    For now, route all `msg_show` to `popup`, which is a much better
    experience for `:Inspect`, `:echo` &c.
    
    It's not yet clear to me whether there's any downside here.

--- packages/nvim/config/lua/plugins/ui.lua
+++ packages/nvim/config/lua/plugins/ui.lua
@@ -12,6 +12,8 @@ return {
 
   -- noicer ui
   {
+    -- https://github.com/folke/noice.nvim#%EF%B8%8F-configuration
+    -- https://github.com/folke/noice.nvim#-views
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
@@ -26,7 +28,20 @@ return {
         command_palette = true,
         long_message_to_split = true,
       },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+          },
+          view = "popup",
+        }
+      }
     },
+    config = function(_, opts)
+      local noice = require("noice")
+      noice.setup(opts)
+      noice.redirect("Inspect")
+    end,
     -- stylua: ignore
     keys = {
       { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
