27841ad42fcb7a63c720b133b534bd8dcc78717e
David J. Hamilton <github@hjdivad.com>
2024-01-22 07:32:54 -0800

    feat: configure neotest

--- packages/nvim/config/lua/plugins/extras/test/neotest.lua
+++ packages/nvim/config/lua/plugins/extras/test/neotest.lua
@@ -2,16 +2,16 @@
 return {
   {
     "nvim-neotest/neotest",
-    -- TODO: see what keys i want
-    -- https://www.lazyvim.org/extras/test/core
+    -- see ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/extras/test/core.lua
     keys = {
-      {"<leader>tt", false},
-      {"<leader>tT", false},
-      {"<leader>tr", false},
-      {"<leader>ts", false},
-      {"<leader>to", false},
-      {"<leader>tO", false},
-      {"<leader>tS", false},
+      { "<leader>tt", false },
+      { "<leader>ts", false },
+      { "<leader>tO", false },
+      { "<leader>to", function() require("neotest").output_panel.toggle({enter=true}) end, desc = "Toggle Output Panel" },
+      { "<leader>tf", function()
+        require("neotest").run.run(vim.fn.expand("%"))
+        require("neotest").summary.open()
+      end, desc = "Run File" },
     }
     ,
   },
