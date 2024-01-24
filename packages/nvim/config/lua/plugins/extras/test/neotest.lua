493f59f30453792d054db61e3812d6126452293e
David J. Hamilton <github@hjdivad.com>
2024-01-23 16:11:12 -0800

    feat: configure neotest

--- packages/nvim/config/lua/plugins/extras/test/neotest.lua
+++ packages/nvim/config/lua/plugins/extras/test/neotest.lua
@@ -3,15 +3,27 @@ return {
   {
     "nvim-neotest/neotest",
     -- see ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/extras/test/core.lua
+    opts = {
+      -- --- @type neotest.Config.summary
+      -- summary = {},
+      -- diagnostic config is not working
+      -- ---@type neotest.Config.diagnostic
+      -- diagnostic = { enabled = true, severity = vim.diagnostic.severity.ERROR },
+      ---@type neotest.Config.output
+      output = { enabled = true, open_on_run = false }
+    },
     keys = {
       { "<leader>tt", false },
       { "<leader>ts", false },
-      { "<leader>tO", false },
-      { "<leader>to", function() require("neotest").output_panel.toggle({enter=true}) end, desc = "Toggle Output Panel" },
-      { "<leader>tf", function()
-        require("neotest").run.run(vim.fn.expand("%"))
-        require("neotest").summary.open()
-      end, desc = "Run File" },
+      { "<leader>tS", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+          require("neotest").summary.open()
+        end,
+        desc = "Run File"
+      },
     }
     ,
   },
