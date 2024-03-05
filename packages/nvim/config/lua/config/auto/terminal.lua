ba58eefa45317ab50e9fb8bbd4fb1959bb7c8eca
David J. Hamilton <dhamilton@linkedin.com>
2024-03-04 16:49:09 -0800

    feat: Improve terminal usage
    Instead of having a terminal on the left, have two kinds of terminals:
    
    * The floating terminal from ctrl-t
    * Terminals in a separate tab for long-running processes (e.g. servers)
    
    To make this more convenient, configure kitty + neovim so that ⌘-n goes
    to tab n and ⌘-t creates a new (neovim) tab.  This prevents me from
    using kitty tabs, which is fine since I never want to do that.

--- packages/nvim/config/lua/config/auto/terminal.lua
+++ packages/nvim/config/lua/config/auto/terminal.lua
@@ -10,15 +10,9 @@ function M.setup_terminal_window()
 
   win.number = false
   win.relativenumber = false
-  win.winfixwidth = true
 
   local win_config = vim.api.nvim_win_get_config(win_id)
   local is_not_float = win_config.relative == ""
-
-  if is_not_float then
-    -- TODO: calculate window width
-    vim.api.nvim_win_set_width(win_id, 100)
-  end
 end
 
 function M.setup()
--- packages/nvim/config/lua/config/keymaps.lua
+++ packages/nvim/config/lua/config/keymaps.lua
@@ -2,8 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- TODO: rm sticky-term in favour of float term
-local term = require("sticky-term")
 local LazyVimUtil = require("lazyvim.util")
 
 local function show_wikilinks_log()
@@ -12,7 +10,7 @@ end
 
 local ks = vim.keymap.set
 local existing_keymaps_lhs = {}
-local kd = function (mode, lhs)
+local kd = function(mode, lhs)
   local normalized_lhs = string.gsub(lhs, "<leader>", vim.g.mapleader)
   local km_lhs = existing_keymaps_lhs[mode]
   if not km_lhs then
@@ -71,8 +69,8 @@ kd("n", "<leader>uF")
 kd("n", "<leader>ut")
 kd("n", "<leader>uT")
 kd("n", ";")
-kd("t", "<C-l>");
-kd("t", "<C-L>");
+kd("t", "<C-l>")
+kd("t", "<C-L>")
 
 -- I have no idea what map("n", "gw", "*N") is supposed to do
 kd("n", "gw")
@@ -89,7 +87,7 @@ kd("v", "<C-S>")
 kd("n", "<C-S>")
 kd("s", "<C-S>")
 
-kd("n", "<leader>l")  -- lazy
+kd("n", "<leader>l") -- lazy
 kd("n", "<leader>fn") -- new file
 kd("n", "<leader>xl") -- location list
 kd("n", "<leader>xq") -- quickfix list
@@ -110,19 +108,19 @@ if vim.fn.has("nvim-0.9.0") == 1 then
   kd("n", "<leader>ui") -- inspect pos; no idea what this does
 end
 
-kd("n", "<leader>ft")         -- floating terminal cwd=get_root()
-kd("n", "<leader>fT")         -- floating terminal
-kd("n", "<leader>ww")         -- other window
-kd("n", "<leader>wd")         -- delete window
-kd("n", "<leader>w-")         -- split below
-kd("n", "<leader>w|")         -- split right
-kd("n", "<leader>-")          -- split below
-kd("n", "<leader><Tab>l")     -- last tab
-kd("n", "<leader><Tab>f")     -- first tab
+kd("n", "<leader>ft") -- floating terminal cwd=get_root()
+kd("n", "<leader>fT") -- floating terminal
+kd("n", "<leader>ww") -- other window
+kd("n", "<leader>wd") -- delete window
+kd("n", "<leader>w-") -- split below
+kd("n", "<leader>w|") -- split right
+kd("n", "<leader>-") -- split below
+kd("n", "<leader><Tab>l") -- last tab
+kd("n", "<leader><Tab>f") -- first tab
 kd("n", "<leader><Tab><Tab>") -- new tab
-kd("n", "<leader><Tab>]")     -- next tab
-kd("n", "<leader><Tab>d")     -- delete tab
-kd("n", "<leader><Tab>[")     -- previous tab
+kd("n", "<leader><Tab>]") -- next tab
+kd("n", "<leader><Tab>d") -- delete tab
+kd("n", "<leader><Tab>[") -- previous tab
 
 -- add keymaps
 
@@ -137,6 +135,28 @@ ks("n", "<leader>7", "7gt", { desc = "go to tab 7" })
 ks("n", "<leader>8", "8gt", { desc = "go to tab 8" })
 ks("n", "<leader>9", "9gt", { desc = "go to tab 9" })
 ks("n", "<leader>0", "0gt", { desc = "go to tab 10" })
+ks("t", "<F1>", "<Cmd>tabn 1<cr>", { desc = "go to tab 1" })
+ks("t", "<F2>", "<Cmd>tabn 2<cr>", { desc = "go to tab 2" })
+ks("t", "<F3>", "<Cmd>tabn 3<cr>", { desc = "go to tab 3" })
+ks("t", "<F4>", "<Cmd>tabn 4<cr>", { desc = "go to tab 4" })
+ks("t", "<F5>", "<Cmd>tabn 5<cr>", { desc = "go to tab 5" })
+ks("t", "<F6>", "<Cmd>tabn 6<cr>", { desc = "go to tab 6" })
+ks("t", "<F7>", "<Cmd>tabn 7<cr>", { desc = "go to tab 7" })
+ks("t", "<F8>", "<Cmd>tabn 8<cr>", { desc = "go to tab 8" })
+ks("t", "<F9>", "<Cmd>tabn 9<cr>", { desc = "go to tab 9" })
+ks("t", "<F10>", "<Cmd>tabn 10<cr>", { desc = "go to tab 10" })
+ks("t", "<F11>", "<Cmd>tabnew<cr>", { desc = "create a new tab" })
+ks("n", "<F1>", "<Cmd>tabn 1<cr>", { desc = "go to tab 1" })
+ks("n", "<F2>", "<Cmd>tabn 2<cr>", { desc = "go to tab 2" })
+ks("n", "<F3>", "<Cmd>tabn 3<cr>", { desc = "go to tab 3" })
+ks("n", "<F4>", "<Cmd>tabn 4<cr>", { desc = "go to tab 4" })
+ks("n", "<F5>", "<Cmd>tabn 5<cr>", { desc = "go to tab 5" })
+ks("n", "<F6>", "<Cmd>tabn 6<cr>", { desc = "go to tab 6" })
+ks("n", "<F7>", "<Cmd>tabn 7<cr>", { desc = "go to tab 7" })
+ks("n", "<F8>", "<Cmd>tabn 8<cr>", { desc = "go to tab 8" })
+ks("n", "<F9>", "<Cmd>tabn 9<cr>", { desc = "go to tab 9" })
+ks("n", "<F10>", "<Cmd>tabn 10<cr>", { desc = "go to tab 10" })
+ks("n", "<F11>", "<Cmd>tabnew<cr>", { desc = "create a new tab" })
 
 ks("n", "<m-;>", ":lua =", { desc = "lua cmdline" })
 ks("n", "<c-n>", ":lua =require('noice').redirect([[]])<Left><Left><Left>", { desc = "noice cmdline" })
@@ -152,20 +172,18 @@ ks(
   { desc = "Redraw / clear hlsearch / checktime / diff update" }
 )
 
-
 local lazyterm = function()
   LazyVimUtil.terminal(nil, {
     cwd = LazyVimUtil.root(),
     border = "rounded",
     size = {
       height = 0.5,
-      width = 0.5
-    }
+      width = 0.5,
+    },
   })
 end
 -- TODO: need another one that opens at &pwd
 ks("n", "<c-t>", lazyterm, { desc = "Terminal (root dir)" })
-
 
 local clipboard_reg
 if vim.fn.has("clipboard") then
@@ -192,12 +210,11 @@ ks("t", "<c-w>l", [[<c-\><c-n><c-w>l]], { desc = "win-right (terminal)" })
 ks("t", "<c-w><c-l>", [[<c-\><c-n><c-w>l]], { desc = "win-right (terminal)" })
 ks("t", "<c-w>n>", [[<c-\><c-n><c-w>n<Cmd>terminal<cr>]], { desc = "win new (terminal)" })
 ks("t", "<c-w><c-n>", [[<c-\><c-n><c-w>n<Cmd>terminal<cr>]], { desc = "win new (terminal)" })
+ks("t", "<c-w>v>", [[<c-\><c-n><c-w>v<Cmd>terminal<cr>]], { desc = "win new (vertical terminal)" })
+ks("t", "<c-w><c-v>", [[<c-\><c-n><c-w>v<Cmd>terminal<cr>]], { desc = "win new (vertical terminal)" })
 ks("t", "<c-w>c", [[<c-\><c-n><c-w>c]], { desc = "win-right (terminal)" })
 ks("t", "<c-w><c-c>", [[<c-\><c-n><c-w>c]], { desc = "win-right (terminal)" })
 
-ks("n", "<leader>tt", term.goto_terminal, { desc = "go to terminal" })
---TODO: <leader>tq close terminal win
-ks("n", "<leader>ult", term.show_logs, { desc = "show logs (sticky-term)" })
 ks("n", "<leader>ulw", show_wikilinks_log, { desc = "show logs (cmp-wikilinks)" })
 ks("n", "<leader>uld", "<Cmd>DapShowLog<Cr>", { desc = "show logs (DAP)" })
 ks("n", "<leader>uL", function()
