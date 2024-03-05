ca27977fe336c84521153925c13a9f85199a7fcf
David J. Hamilton <dhamilton@linkedin.com>
2024-03-05 15:40:12 -0800

    fix: delete c-{j,k} in terminal

--- packages/nvim/config/lua/config/keymaps.lua
+++ packages/nvim/config/lua/config/keymaps.lua
@@ -71,6 +71,8 @@ kd("n", "<leader>uT")
 kd("n", ";")
 kd("t", "<C-l>")
 kd("t", "<C-L>")
+kd("t", "<C-K>")
+kd("t", "<C-J>")
 
 -- I have no idea what map("n", "gw", "*N") is supposed to do
 kd("n", "gw")
