3fb9b5f29aa40eb9a2b8cca8321d15634270f818
David J. Hamilton <github@hjdivad.com>
2024-03-09 13:24:03 -0800

    feat: mason ensure things are installed

--- packages/nvim/config/lua/plugins/lsp.lua
+++ packages/nvim/config/lua/plugins/lsp.lua
@@ -6,68 +6,104 @@ local function diagnostic_goto(next, severity)
   end
 end
 
----@type LazyPluginSpec
+---@type LazyPluginSpec[]
 return {
-  -- see https://www.lazyvim.org/plugins/lsp
-  "neovim/nvim-lspconfig",
-  -- override LSP keybindings here
-  init = function()
-    -- see https://www.lazyvim.org/plugins/lsp#%EF%B8%8F-customizing-lsp-keymaps
-    -- see $HOME/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/lsp/keymaps.lua
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- lsp
+        "lua-language-server",
+        "marksman",
+        "pyright",
+        "ruff-lsp",
+        "rust-analyzer",
+        "sqlls",
+        "typescript-language-server",
+        "yaml-language-server",
+        "json-lsp",
 
-    -- remove unwanted
-    keys[#keys + 1] = { "gy", false }
-    keys[#keys + 1] = { "gI", false }
-    keys[#keys + 1] = { "gd", false }
-    keys[#keys + 1] = { "<leader>cs", false }
-    keys[#keys + 1] = { "<c-k>", false, mode = "i" }
-    keys[#keys + 1] = { "<C-k>", false, mode = "i" }
+        -- dap
+        "codelldb",
+        "debugpy",
 
-    -- add missing
-    keys[#keys + 1] = {
-      "gt",
-      function()
-        require("telescope.builtin").lsp_type_definitions()
-      end,
-      desc = "Goto [T]ype Definition",
-    }
+        -- linters
+        "flake8",
+        "shellcheck",
+        "commitlint",
 
-    keys[#keys + 1] = {
-      "gd",
-      function() require("telescope.builtin").lsp_definitions() end,
-      desc =
-      "Goto Definition",
-      has = "definition"
-    }
+        -- formatters
+        "fixjson",
+        "markdownlint",
+        "shfmt",
+        "stylua",
+      },
+    },
+  },
+  {
+    -- see https://www.lazyvim.org/plugins/lsp
+    "neovim/nvim-lspconfig",
+    -- override LSP keybindings here
+    init = function()
+      -- see https://www.lazyvim.org/plugins/lsp#%EF%B8%8F-customizing-lsp-keymaps
+      -- see $HOME/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/lsp/keymaps.lua
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
-    keys[#keys + 1] = {
-      "gi",
-      function()
-        require("telescope.builtin").lsp_implementations()
-      end,
-      desc = "Goto Implementation",
-    }
+      -- remove unwanted
+      keys[#keys + 1] = { "gy", false }
+      keys[#keys + 1] = { "gI", false }
+      keys[#keys + 1] = { "gd", false }
+      keys[#keys + 1] = { "<leader>cs", false }
+      keys[#keys + 1] = { "<c-k>", false, mode = "i" }
+      keys[#keys + 1] = { "<C-k>", false, mode = "i" }
 
-    keys[#keys + 1] = { "<leader>cL", vim.diagnostic.open_float, desc = "Line Diagnostics" }
-    keys[#keys + 1] = { "<leader>cj", diagnostic_goto(true, "ERROR"), desc = "Next Error Diagnostic" }
-    keys[#keys + 1] = { "<leader>ck", diagnostic_goto(false, "ERROR"), desc = "Prev Error Diagnostic" }
-    keys[#keys + 1] = { "<leader>cJ", diagnostic_goto(true), desc = "Next Diagnostic" }
-    keys[#keys + 1] = { "<leader>cK", diagnostic_goto(false), desc = "Prev Diagnostic" }
-    keys[#keys + 1] = { "<leader>cs", "<cmd>AerialToggle!<cr>", desc = "Code Symbols" }
-    keys[#keys + 1] = { "<leader>cS", "<cmd>AerialOpen<cr>", desc = "Code Symbols (focus)" }
-    keys[#keys + 1] = { "<c-h>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" }
-  end,
-  ---@type PluginLspOpts
-  opts = {
-    -- see https://www.lazyvim.org/plugins/lsp#nvim-lspconfig / Full Spec
-    --
-    -- LSP-specific keymaps here
-    --
-    -- servers = {
-    --   lua_ls = {
-    --     keys = {}
-    --   }
-    -- }
+      -- add missing
+      keys[#keys + 1] = {
+        "gt",
+        function()
+          require("telescope.builtin").lsp_type_definitions()
+        end,
+        desc = "Goto [T]ype Definition",
+      }
+
+      keys[#keys + 1] = {
+        "gd",
+        function()
+          require("telescope.builtin").lsp_definitions()
+        end,
+        desc = "Goto Definition",
+        has = "definition",
+      }
+
+      keys[#keys + 1] = {
+        "gi",
+        function()
+          require("telescope.builtin").lsp_implementations()
+        end,
+        desc = "Goto Implementation",
+      }
+
+      keys[#keys + 1] = { "<leader>cL", vim.diagnostic.open_float, desc = "Line Diagnostics" }
+      keys[#keys + 1] = { "<leader>cj", diagnostic_goto(true, "ERROR"), desc = "Next Error Diagnostic" }
+      keys[#keys + 1] = { "<leader>ck", diagnostic_goto(false, "ERROR"), desc = "Prev Error Diagnostic" }
+      keys[#keys + 1] = { "<leader>cJ", diagnostic_goto(true), desc = "Next Diagnostic" }
+      keys[#keys + 1] = { "<leader>cK", diagnostic_goto(false), desc = "Prev Diagnostic" }
+      keys[#keys + 1] = { "<leader>cs", "<cmd>AerialToggle!<cr>", desc = "Code Symbols" }
+      keys[#keys + 1] = { "<leader>cS", "<cmd>AerialOpen<cr>", desc = "Code Symbols (focus)" }
+      keys[#keys + 1] =
+        { "<c-h>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" }
+    end,
+    ---@type PluginLspOpts
+    opts = {
+      -- see https://www.lazyvim.org/plugins/lsp#nvim-lspconfig / Full Spec
+      --
+      -- LSP-specific keymaps here
+      --
+      -- servers = {
+      --   lua_ls = {
+      --     keys = {}
+      --   }
+      -- }
+    },
   },
 }
