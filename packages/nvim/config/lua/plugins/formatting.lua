b0e4a8093e935279c7f81b9eb6129f73a43dbe41
David J. Hamilton <github@hjdivad.com>
2024-03-21 16:20:08 -0700

    fix: configure conform to use rustfmt
    A recent LazyVim bump always tries to use conform over lsp formatting if
    it can be required.  In practice this means I have to configure conform
    to format every filetype I want formatted as the lsp fallback in
    practice doesn't work with conform installed.

--- packages/nvim/config/lua/plugins/formatting.lua
+++ packages/nvim/config/lua/plugins/formatting.lua
@@ -11,6 +11,7 @@ return {
           markdown = { "prettier" },
           json = { "fixjson" },
           lua = { "stylua" },
+          rust = { "rustfmt" },
         },
       }
       return opts
