3b63ca043b434bb68a9315099902e301e081b0be
David J. Hamilton <github@hjdivad.com>
2024-05-11 06:58:06 -0700

    feat: luarocks deps

---
+++ packages/nvim/config/lua/plugins/extras/rocks/luarocks.lua
@@ -0,0 +1,11 @@
+---@type LazySpec[]
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    opts = {
+      rocks = {  }, -- specifies a list of rocks to install
+      -- luarocks_build_args = { "--with-lua=/my/path" }, -- extra options to pass to luarocks's configuration script
+    },
+  },
+}
