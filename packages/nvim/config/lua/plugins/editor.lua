e0c2c3813f36dff32e2e7597b7a8b0834fd6b436
David J. Hamilton <git@david.hamilton.gg>
2024-08-06 08:07:22 -0700

    chore: formatting

--- packages/nvim/config/lua/plugins/editor.lua
+++ packages/nvim/config/lua/plugins/editor.lua
@@ -117,12 +117,12 @@ return {
     opts = {
       base = "origin/HEAD",
       signs = {
-        add          = { text = '│' },
-        change       = { text = '│' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked    = { text = '┆' },
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
       },
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
