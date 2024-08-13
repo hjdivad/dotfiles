01939b16fc322c2760668b2c8a7353ce51d403f6
David J. Hamilton <dhamilton@linkedin.com>
2024-08-13 15:20:35 -0700

    fix: slow shell init
    - pyenv is really slow
    - rbenv is a bit slow and i rely on it so infrequently having it be
      JIT is fine
    
    The issue is that "deferred plugins" doesn't really work.  It *does*
    improve the literal shell startup time, but it doesn't improve
    time-to-interactive, making it a false savings.

--- packages/zsh/plugins.deferred/pyenv.zsh
+++ packages/zsh/plugins.deferred/pyenv.zsh
@@ -1,4 +1,6 @@
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# TODO: don't do this every shell, it is unacceptably slow
+#
+# export PYENV_ROOT="$HOME/.pyenv"
+# [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
--- packages/zsh/plugins.deferred/rbenv.zsh
+++ packages/zsh/plugins.deferred/rbenv.zsh
@@ -1,1 +1,19 @@
-# eval "$(rbenv init -)"
+# TODO: do this jit with aliases
+#   - gem
+#   - ruby
+#   - $HOME/.rbenv/shims/*
+
+function rbenv_init {
+  eval "$(rbenv init -)"
+
+  unalias gem
+  unalias ruby
+  unalias pws
+  unfunction rbenv_init
+
+  echo "rbenv init ran; re-run your command"
+}
+
+alias gem=rbenv_init
+alias ruby=rbenv_init
+alias pws=rbenv_init
