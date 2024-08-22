fbfbe89e2c2a2bb869f48326b3e70c692c421c03
David J. Hamilton <git@david.hamilton.gg>
2024-08-22 08:02:56 -0700

    feat(start): configure start-tmux
    I can't use it yet b/c I need the fixed version, which should be
    extracted to shared_binutils.

---
+++ packages/binutils/config/config.lua
@@ -0,0 +1,110 @@
+---@type Config
+return {
+	tmux = {
+		sessions = {
+			{
+				name = "📋 todos",
+				windows = {
+					{
+						name = "todos",
+						path = "~/docs/vadnu/home",
+						command = "nvim",
+					},
+
+					{
+						name = "reference",
+						path = "~/docs/vadnu/home/ref",
+						command = "nvim",
+					},
+
+					{
+						name = "journal",
+						path = "~/docs/vadnu/home/journal",
+						command = "nvim",
+					},
+				},
+			},
+
+			{
+				name = "🟡 dotfiles",
+				windows = {
+					{
+						name = "dotfiles",
+						path = "~/src/github/hjdivad/dotfiles",
+						command = "nvim",
+					},
+
+					{
+						name = "binutils",
+						path = "~/src/github/hjdivad/dotfiles/packages/binutils",
+						command = "nvim",
+					},
+
+					{
+						name = "shared-binutils",
+						path = "~/src/github/malleatus/shared_binutils/",
+						command = "nvim",
+					},
+
+					{
+						name = "rwjblue/dotfiles",
+						path = "~/src/github/rwjblue/dotfiles",
+						command = "nvim",
+					},
+
+					{
+						name = "rwjblue/dotvim",
+						path = "~/src/github/rwjblue/dotvim",
+						command = "nvim",
+					},
+				},
+			},
+
+			{
+				name = "💪 ud_macros",
+				windows = {
+					{
+						name = "ud_macros",
+						path = "~/src/github/hjdivad/ud_macros",
+						command = "nvim",
+					},
+
+					{
+						name = "sandbox-kotlin",
+						path = "~/src/nas/hjdivad/sandbox-kotlin",
+						command = "nvim",
+					},
+
+					{
+						name = "sandbox-android",
+						path = "~/src/nas/hjdivad/sandbox-android",
+						command = "nvim",
+					},
+
+					{
+						name = "sandbox-android2",
+						path = "~/src/nas/hjdivad/sandbox-android",
+						command = "nvim",
+					},
+				},
+			},
+
+			{
+				name = "🦾 montoya",
+				windows = {
+					{
+						name = "montoya",
+						path = "~/src/github/malleatus/montoya.nvim",
+						command = "nvim",
+					},
+
+					{
+						name = "montoya2",
+						path = "~/src/github/malleatus/montoya.nvim",
+						command = "nvim",
+					},
+				},
+			},
+		},
+	},
+}
