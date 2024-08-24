86f9adf3b82e95679d568c63abf2e7a8b4221c47
David J. Hamilton <git@david.hamilton.gg>
2024-08-24 16:24:59 -0700

    feat: use hyper-n to switch to tmux window n

--- packages/wezterm/wezterm.lua
+++ packages/wezterm/wezterm.lua
@@ -16,10 +16,11 @@ config.window_padding = { left = 2, right = 2, top = 2, bottom = 2 }
 config.native_macos_fullscreen_mode = false
 
 -- use this along with Ctrl-Shift-L to see the debug messages for what actual keycodes were detected
---config.debug_key_events = true
-
+config.debug_key_events = true
 
 config.keys = {}
+
+-- Use ⌘-n to go to tab n in neovim
 for i = 1, 9 do
 	-- {
 	-- 	key = "1",
@@ -32,6 +33,88 @@ for i = 1, 9 do
 		action = wezterm.action({ SendKey = { key = "F" .. i } }),
 	})
 end
+
+-- Use hyper-n to go to window n in tmux
+table.insert(config.keys, {
+	key = '!',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '1' }),
+	}),
+})
+table.insert(config.keys, {
+	key = '@',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '2' }),
+	}),
+})
+table.insert(config.keys, {
+	key = '#',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '3' }),
+	}),
+})
+table.insert(config.keys, {
+	key = '$',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '4' }),
+	}),
+})
+table.insert(config.keys, {
+	key = '%',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '5' }),
+	}),
+})
+table.insert(config.keys, {
+	key = '^',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '6' }),
+	}),
+})
+table.insert(config.keys, {
+	key = '&',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '7' }),
+	}),
+})
+table.insert(config.keys, {
+	key = '*',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '8' }),
+	}),
+})
+table.insert(config.keys, {
+	key = '(',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '9' }),
+	}),
+})
+table.insert(config.keys, {
+	key = ')',
+	mods = "SHIFT|ALT|CTRL|SUPER",
+	action = wezterm.action.Multiple({
+		wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
+		wezterm.action.SendKey({ key = '0' }),
+	}),
+})
 
 config.initial_rows = 90
 config.initial_cols = 300
