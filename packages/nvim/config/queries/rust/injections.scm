d3e254bc7ac5dfa29bf50726c3e2963dd2e76f2f
David J. Hamilton <git@david.hamilton.gg>
2024-10-06 16:24:15 -0700

    feat: example lang injection
    It looks like the rust TS grammars have changed, but it was easy to get
    a query that injects lua.  Unfortunately it conflicts with rust-analyzer
    for some reason.

--- packages/nvim/config/queries/rust/injections.scm
+++ packages/nvim/config/queries/rust/injections.scm
@@ -10,3 +10,21 @@
 ;     (string_literal) @injection.content)
 ;   (#offset! @injection.content 0 1 0 -1)
 ;   (#set! injection.language "sql"))
+;
+
+; TODO: This works but rust-analyzer clears the highlighting after it
+; initializes.
+;
+; (macro_invocation
+;   macro: [
+;     (scoped_identifier
+;       name: (_) @_macro_name)
+;     (identifier) @_macro_name
+;   ]
+;   (token_tree
+;     (raw_string_literal
+;       (string_content) @injection.content))
+;   (#eq? @_macro_name "lua")
+;   (#offset! @injection.content 0 1 0 -1)
+;   (#set! injection.language "lua")
+;   (#set! injection.include-children))
