12ca27dc87b908651653df71446723d90f65306d
David J. Hamilton <git@david.hamilton.gg>
2025-02-14 07:52:46 -0800

    chore: drop stale config
    The defaults for conform + lazyvim work better now.

--- packages/nvim/config/lua/plugins/formatting.lua
+++ packages/nvim/config/lua/plugins/formatting.lua
@@ -1,17 +1,4 @@
 ---@type LazySpec[]
 return {
   -- see https://www.lazyvim.org/plugins/formatting
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        markdown = { { "prettierd", "prettier" } },
-        json = { "fixjson" },
-        lua = { "stylua" },
-        rust = { "rustfmt" },
-        kotlin = { "ktfmt" },
-        python = {},
-      },
-    },
-  },
 }
