f46f9ceecb6a2f15e31c0f206194014d1c3a88cf
David J. Hamilton <david.hamilton@glean.com>
2025-04-11 14:30:29 -0700

    fix: disable d2
    This plugin sets `syntax on` which causes loads of problems.  If I end
    up doing a lot of d2 again I'll probably want to write a treesitter
    grammar.

--- packages/nvim/lua/plugins/extras/d2/lang.lua
+++ packages/nvim/lua/plugins/extras/d2/lang.lua
@@ -1,7 +1,16 @@
+--FIXME: Enabling this extra causes a lot of problems because it sets
+--vim.opt.syntax = true
+--
+--You really don't want this for all kinds of languages (e.g. markdown, java)
+--Realistically the thing to do is to write a treesitter grammar for d2.
+
 ---@type LazySpec
-return {
-  --TODO: I'd rather use a TreeSitter grammar
-  --have to :syntax on for .d2 files
-  "terrastruct/d2-vim",
-  lazy = false
-}
+-- return {
+--   --TODO: I'd rather use a TreeSitter grammar
+--   --have to :syntax on for .d2 files
+--   "terrastruct/d2-vim",
+--   lazy = false
+-- }
+
+---@type LazySpec
+return {}
