a054640605f3cacb00fcbf0fd41947a603c83e6b
David J. Hamilton <david.hamilton@glean.com>
2025-04-29 08:26:35 -0700

    feat(formatting): prefer prettier over lsp

--- packages/nvim/lua/plugins/formatting.lua
+++ packages/nvim/lua/plugins/formatting.lua
@@ -1,4 +1,12 @@
 ---@type LazySpec[]
 return {
   -- see https://www.lazyvim.org/plugins/formatting
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typescript = { "prettier" },
+      },
+    },
+  },
 }
