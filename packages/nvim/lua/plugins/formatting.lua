62d894d3788710ba1dbbf835d09b5ce39020a5e2
David J. Hamilton <david.hamilton@glean.com>
2025-05-12 12:15:51 -0700

    feat(go): formatting

--- packages/nvim/lua/plugins/formatting.lua
+++ packages/nvim/lua/plugins/formatting.lua
@@ -4,8 +4,28 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+
+      -- Expected to be paired with $REPOROOT/.lazy.lua
+      --
+      -- return {
+      --   "stevearc/conform.nvim",
+      --   opts = {
+      --     formatters = {
+      --       goimports = {
+      --         args = { "--local", "github.com/my/cool/repo" },
+      --       },
+      --       golines = {
+      --        args = { "--max-len", 100 }
+      --       }
+      --     },
+      --     formatters_by_ft = {
+      --       go = { "golines", "goimports", "gofmt" },
+      --     },
+      --   },
+      -- }
       formatters_by_ft = {
         typescript = { "prettier" },
+        go = { "golines", "goimports", "gofmt" },
       },
     },
   },
