2259ed9d1b5da8fadaf38866c1eb40a2b9651abc
David J. Hamilton <david.hamilton@glean.com>
2025-08-12 16:31:16 -0700

    feat(snippets): add prompt snippets
    Note this doesn't quite work yet with my startup function because
    BufNewFile is not triggered during VimEnter.

--- packages/nvim/lua/config/autocmds.lua
+++ packages/nvim/lua/config/autocmds.lua
@@ -125,6 +125,16 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   end,
 })
 
+--TODO: this doesn't work properly because BufNewFile is not called during
+--VimEnter, see :he VimEnter
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "**/*.prompt.md" },
+  group = ft_group,
+  callback = function()
+    require("luasnip").filetype_extend("markdown", { "prompt" })
+  end,
+})
+
 term.setup()
 
 
