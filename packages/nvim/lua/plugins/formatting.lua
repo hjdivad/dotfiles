579c847a2c0b8f5bbe906c7b6160d7cb9252f8fd
David J. Hamilton <david.hamilton@glean.com>
2025-10-14 17:10:47 -0700

    fix(sql): formatting

--- packages/nvim/lua/plugins/formatting.lua
+++ packages/nvim/lua/plugins/formatting.lua
@@ -23,6 +23,12 @@ return {
       --     },
       --   },
       -- }
+      formatters = {
+        -- Configure sqlfluff formatter to run from git root
+        sqlfluff = {
+          cwd = require("conform.util").root_file(".sqlfluff", ".git"),
+        },
+      },
       formatters_by_ft = {
         typescript = { "prettier" },
         go = { "golines", "goimports", "gofmt" },
