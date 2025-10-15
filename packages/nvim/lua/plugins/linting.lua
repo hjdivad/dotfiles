89e58be9a7aa234113fa6ff8dc607e8fe60cf0cd
David J. Hamilton <david.hamilton@glean.com>
2025-10-14 17:11:12 -0700

    fix(sql): lint config for sqlfluff

--- packages/nvim/lua/plugins/linting.lua
+++ packages/nvim/lua/plugins/linting.lua
@@ -2,4 +2,13 @@
 return {
   -- see https://www.lazyvim.org/plugins/linting
   -- see :help nvim-lint
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      local fluff = require('lint').linters.sqlfluff
+      fluff.args = { "lint", "--format=json" }
+      return opts
+    end,
+  },
 }
